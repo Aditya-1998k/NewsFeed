e929d4fac90b171cb2c422f3ea87316de5d2af2c
Aditya Gupta <102688268+Aditya-1998k@users.noreply.github.com>
2025-08-31 09:58:11 +0530

    including news routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
+"""Imports Start"""
 from fastapi import FastAPI
+from .routes import news
+"""Imports complete"""
 
 app = FastAPI(title="Newsfeed App")
+
+
+# register your routes here
+app.include_router(news.router)
 
 @app.get("/")
 async def root():
